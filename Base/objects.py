c53ad3979871c53ed04e208e7bca7e25fea48ef2
L <L862608263@163.com>
2018-05-14 11:00:40 +0800

    inherit

--- Base/objects.py
+++ Base/objects.py
@@ -6,6 +6,9 @@
 # @File    : objects.py
 # @Software: PyCharm
 
+
+import random
+
 # 对象 = 属性 + 方法
 
 
@@ -28,6 +31,8 @@ class Turtle:  # Py 中的类名约定以大写字母开头
 
 
 # 继承  ()里面写父类
+# 如果子类中定义与父类相同的方法或属性,则会自动覆盖父类对应的方法属性
+#     子类      (基类、父类或超类)
 class SonTurtle(Turtle):
     pass  # pass 代表不做任何事情
 
@@ -100,12 +105,91 @@ ball.kick()
 
 # name mangling (名字改编,名字重整) 私有属性的内部实现
 print(ball.get_private_property())
-# 把双__的私有变量自动改了名  改成(_类名__变量名)
+# 把双__的私有变量自动改了名  改成(_类名__变量名)(_类名__方法名())
 # python 的私有机制,是伪私有
-print(ball._Ball__private_property)
+# print(ball._Ball__private_property)
 
 # 公有和私有
 
 # 默认的函数变量都是公有的
 
 # 在py中定义私有变量只需在变量或函数名前面加"__"两个下划线,那么这个函数或变量就会成为私有的了
+
+
+# demo
+
+class Fish:
+
+    def __init__(self):
+        self.x = random.randint(0, 10)
+        self.y = random.randint(0, 10)
+
+    def move(self):
+        self.x -= 1
+        print('location: ', self.x, self.y)
+
+
+class GoldFish(Fish):
+    pass
+
+
+class Carp(Fish):
+    pass
+
+
+class Salmon(Fish):
+    pass
+
+
+class Shark(Fish):
+
+    def __init__(self):
+        # 这里重写了父类的init方法,会导致没有x属性
+        # 解决方法有2种
+        # 1, 调用未绑定的父类方法
+        #    Fish.__init__(self)  # Fish.__init__(调用父类的方法),这个self是Shark(子类的实例对象)
+        # 2, 使用super函数
+        super().__init__()
+        self.hungry = True
+
+    def eat(self):
+        print("好吃" if self.hungry else "吃太多了")
+        self.hungry = not self.hungry
+
+
+fish = Fish()
+fish.move()
+
+gold_fish = GoldFish()
+gold_fish.move()
+
+shark = Shark()
+# 调用未绑定的父类方法相当于进行了以下操作
+# Fish.__init__(shark)
+shark.move()
+
+
+# python 支持多重继承
+
+
+class BaseClassOne:
+
+    # noinspection PyMethodMayBeStatic
+    def say_one(self):
+        print('i`m BaseClassOne')
+
+
+class BaseClassTwo:
+
+    # noinspection PyMethodMayBeStatic
+    def say_two(self):
+        print('i`m BaseClassTwo')
+
+
+class SomeClass(BaseClassOne, BaseClassTwo):
+    pass
+
+
+some_class = SomeClass()
+some_class.say_one()
+some_class.say_two()
